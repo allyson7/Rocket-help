098b2dd0c4e256a956d7525808d241681162b683
allyson7 <allysonlopes0@gmail.com>
2022-07-19 15:52:36 -0400

    :wrench: Updating initial configuration

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { THEME } from "./styles/theme";
+
+import { SignIn } from "./src/screens/signin";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <SignIn />
+    </NativeBaseProvider>
+  );
+}
