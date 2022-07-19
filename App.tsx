d29b42cb9f4bcb068f090e9ae2f903d39b264f57
allyson7 <allysonlopes0@gmail.com>
2022-07-19 19:02:15 -0400

    :sparkles: Creating home page

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 
 import { THEME } from "./src/styles/theme";
 
-import { SignIn } from "./src/screens/signin";
+import { Home } from "./src/screens/home";
 import { Loading } from "./src/components/Loading";
 
 export default function App() {
@@ -20,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
